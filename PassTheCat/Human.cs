aca4ad96953e649ff276bee7f8712871cfb708b6
Jfleming <Jfleming07712@gmail.com>
2019-08-01 17:49:25 -0400

    Add project files.

---
+++ PassTheCat/Human.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PassTheCat
+{
+    public class Human
+    {
+        public void DoSomethingWithCat(Delegate passTheCat)
+        {
+            Console.WriteLine("the human pets " + passTheCat. );
+        }
+    }
+}
---
+++ PassTheCat/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PassTheCat
+{
+    class Program
+    {
+        Robot robot = new Robot();
+        Human human = new Human();
+        Bear bear = new Bear();
+        Cat lerroy = new Cat()
+        {
+            Name = "Lerroy",
+            Calories = 200
+        };
+
+        delegate void PassTheCat(string name, int calories);
+
+        
+
+
+        static void Main(string[] args)
+        {
+            PassTheCat DelRobot = new Pass
+
+            
+        }
+    }
+}
